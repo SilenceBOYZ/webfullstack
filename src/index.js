a92c9bc17f3c08ae9a0a735f9b46d311d54c84df
SilenceBOYZ <nguyenthanhtri230799@gmail.com>
2023-12-08 16:37:17 +0700

    Created Api

--- src/index.js
+++ src/index.js
@@ -3,11 +3,14 @@ import bodyParser from 'body-parser';
 import viewEngine from './config/viewEngine';
 import initWebRoutes from './route/web';
 import connectDB from './config/connectDB';
+import cors from 'cors';
+
 require('dotenv').config();
 
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.use(cors({ credentials: true, origin: true }));
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: true}))
 
